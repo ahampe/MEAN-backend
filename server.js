ae70cc093bcb92322ebf719089a196eb2febda30
Alex Hampe <alexanderehampe@gmail.com>
2018-06-14 15:01:46 -0700

    Setup Node Mongoose driver

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var url = "mongodb://localhost:27017/test";
+
+var Message = mongoose.model("Message", {
+    msg: String
+});
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+})
+
 app.post("/api/message", function(req, res) {
     console.log(req.body);
+    
+    var message = new Message(req.body);
+    message.save();
+
     res.status(200);
 })
 
+function GetMessages(){
+    Message.find({}).exec(function(err, result) {
+        console.log(result);
+    })
+}
+
+mongoose.connect(url, function(err){
+    if(!err){
+        console.log("We are connected to Mongo.");
+        GetMessages();
+    }
+})
+
 var server = app.listen(5000, function(){
-    console.log("listening on port ", server.address().port)
+    console.log("listening on port ", server.address().port);
 })
